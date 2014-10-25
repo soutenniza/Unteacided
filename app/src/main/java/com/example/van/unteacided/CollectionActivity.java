e5ac5f454b84739fa901965ea78952f8ee1eeb9a
soutenniza <soutennizavan@gmail.com>
2014-10-25 16:50:10 -0400

    Minor adjustments to UI

--- app/src/main/java/com/example/van/unteacided/CollectionActivity.java
+++ app/src/main/java/com/example/van/unteacided/CollectionActivity.java
@@ -63,7 +63,7 @@ public class CollectionActivity extends SharedActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        //getMenuInflater().inflate(R.menu.collection, menu);
+        getMenuInflater().inflate(R.menu.collection, menu);
         return true;
     }
 
